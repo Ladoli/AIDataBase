62423f4e9b298eb2fe661d0dc1e7345dda5cd4cf
dmennie <32312277+dmennie@users.noreply.github.com>
2018-07-17 10:36:12 -0700

    MYSQL ADDED

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('mysql');
 const cors = require('cors');
 const app            = express();
 
@@ -12,6 +13,17 @@ app.use(cors());
 const port = 8000;
 require('./routes')(app, {});
 
+var con = db.createConnection({
+  host: "my3300db.cpqjuav2elw2.us-west-2.rds.amazonaws.com",
+  user: "masteruser",
+  password: "masterpass",
+  database: "csis3300project"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.listen(port, () => {
   console.log('We are live on the ' + port);
@@ -19,6 +31,15 @@ app.listen(port, () => {
 
 
 app.get('/', function (req, res) {
-  console.log(req.query.query)
-  res.json({"test":"First API"});
+  console.log(req.query.query);
+  //var sql = "SELECT * FROM " + req.query.query + " LIMIT 100";
+  con.query(req.query.query +" LIMIT 5", function (err, result) {
+    if (err) throw err;
+    //console.log("Result: " + JSON.stringify(result[0]));
+    
+    res.json(result);
+
+  });
+
+  
 })
